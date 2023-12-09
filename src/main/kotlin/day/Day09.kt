23750ca882f87d51e0c99b56ffe4f8f541a95cc3
Andi Schröder <andreas.schroeder@iits-consulting.de>
2023-12-09 11:09:49 +0100

    solve part 2 of day 09

--- src/main/kotlin/day/Day09.kt
+++ src/main/kotlin/day/Day09.kt
@@ -1,16 +1,18 @@
 package day
 
+import util.sum
 import util.toLongList
 
 
-object Day09 : Day("09", "114", "6") {
+object Day09 : Day("09", "114", "2") {
     override fun examplePartOne() = getExampleList().parseHistories().nextValue().toString()
-    override fun examplePartTwo() = "getExampleList()"
+    override fun examplePartTwo() = getExampleList().parseHistories().previousValue().toString()
     override fun solvePartOne() = getInputList().parseHistories().nextValue().toString()
-    override fun solvePartTwo() = "getExampleList()"
+    override fun solvePartTwo() = getInputList().parseHistories().previousValue().toString()
 
     private fun List<String>.parseHistories() = map { parseHistory(it) }
     private fun List<OasisHistory>.nextValue() = sumOf { it.nextValue() }
+    private fun List<OasisHistory>.previousValue() = sumOf { it.previousValue() }
 
     private fun parseHistory(line: String) =
         OasisHistory(mutableListOf(OasisHistoryRow(line.toLongList().reversed()))).apply { fill() }
@@ -19,14 +21,18 @@ object Day09 : Day("09", "114", "6") {
         val rows: MutableList<OasisHistoryRow>
     ) {
         fun nextValue() = rows.sumOf { it.historyRow.first() }
-
-        fun isDone() = rows.any { it.isDone() }
+        fun previousValue() =
+            rows.mapIndexed { index, oasisHistoryRow ->
+                oasisHistoryRow.historyRow.last() * if (index % 2 == 0) 1 else -1
+            }.sum()
 
         fun fill() {
             while (!isDone()) {
                 rows.add(OasisHistoryRow(rows.last().nextIteration()))
             }
         }
+
+        private fun isDone() = rows.any { it.isDone() }
     }
 
     data class OasisHistoryRow(
