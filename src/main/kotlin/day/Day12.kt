c5c747df09dfa2ec9c967951086f58417049b127
Andi Schröder <andreas.schroeder@iits-consulting.de>
2023-12-12 18:18:49 +0100

    solve part 2 of day 12

--- src/main/kotlin/day/Day12.kt
+++ src/main/kotlin/day/Day12.kt
@@ -3,37 +3,55 @@ package day
 import util.toIntList
 
 
-object Day12 : Day("12", "21", "") {
-    override fun examplePartOne() = getExampleList().parseSprings().solveOne()
-    override fun examplePartTwo() = "getExampleList()"
-    override fun solvePartOne() = getInputList().parseSprings().solveOne()
-    override fun solvePartTwo() = "getInputList()"
+object Day12 : Day("12", "21", "525152") {
+    override fun examplePartOne() = getExampleList().parseSprings().solve()
+    override fun examplePartTwo() = getExampleList().parseSprings(true).solve()
+    override fun solvePartOne() = getInputList().parseSprings().solve()
+    override fun solvePartTwo() = getInputList().parseSprings(true).solve()
 
-    private fun List<String>.parseSprings() = map { line ->
+    private fun List<String>.parseSprings(multiply: Boolean = false) = map { line ->
         val (springString, damagedString) = line.split(" ")
-        SpringProblem(("$springString.").toList().map { Spring.from(it) }, damagedString.split(",").toIntList())
+        val readySpringString = if (!multiply) springString else List(5) { springString }.joinToString("?")
+        val readyDamagedString = if (!multiply) damagedString else List(5) {damagedString}.joinToString(",")
+        SpringProblem(
+            ("$readySpringString.").toList().map { Spring.from(it) },
+            readyDamagedString.split(",").toIntList()
+        )
     }
 
-    private fun List<SpringProblem>.solveOne() = sumOf { it.getCombinations() }.toString()
+    private fun List<SpringProblem>.solve() = sumOf { it.getCombinations() }.toString()
 
     data class SpringProblem(
         val springs: List<Spring>,
         val damagedSpringAmounts: List<Int>
     ) {
+        private val memory = mutableMapOf<Pair<Int, Int>, Long>()
 
-        fun getCombinations(springIndex: Int = 0, amountIndex: Int = 0): Int {
-            var combinations = 0
-            if (springIndex >= springs.size) {
-                return if (amountIndex == damagedSpringAmounts.size) 1 else 0
+        fun getCombinations(springIndex: Int = 0, amountIndex: Int = 0): Long {
+            if (hasSpringIndexReachedEnd(springIndex)) {
+                return if (hasAmountIndexReachedEnd(amountIndex)) 1 else 0
             }
+
             val currentSpring = springs[springIndex]
-            if (currentSpring.isMaybeWorking()) combinations += getCombinations(springIndex + 1, amountIndex)
-            if (amountIndex == damagedSpringAmounts.size) return combinations
-            val currentAmount = damagedSpringAmounts[amountIndex]
-            if (foundGroup(springIndex, currentAmount)) {
-                combinations += getCombinations(springIndex + currentAmount + 1, amountIndex + 1)
+            var combinations = if (currentSpring.isMaybeWorking()) getMemory(springIndex + 1, amountIndex) else 0
+
+            if (isAmountInBounds(amountIndex)) {
+                val currentAmount = damagedSpringAmounts[amountIndex]
+                if (foundGroup(springIndex, currentAmount)) {
+                    combinations += getMemory(springIndex + currentAmount + 1, amountIndex + 1)
+                }
             }
             return combinations
+        }
+
+        private fun hasAmountIndexReachedEnd(amountIndex: Int) = amountIndex == damagedSpringAmounts.size
+
+        private fun hasSpringIndexReachedEnd(springIndex: Int) = springIndex >= springs.size
+
+        private fun isAmountInBounds(amountIndex: Int) = amountIndex < damagedSpringAmounts.size
+
+        private fun getMemory(springIndex: Int, amountIndex: Int) = memory.getOrPut(springIndex to amountIndex) {
+            getCombinations(springIndex, amountIndex)
         }
 
         private fun foundGroup(springIndex: Int, currentAmount: Int) =
